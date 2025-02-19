3e13f1c6f4234609279b8b488dcde9c0ff8e864a
Ismail Abdifadil <139770557+ismailabdifadil@users.noreply.github.com>
2025-02-19 13:02:33 +0300

    cors all

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,7 @@ import cors from 'cors'
 
 const app = express()
 app.use(express.json())
-app.use(cors({
-  origin: 'https://mern-auth-frontend-2jo3.onrender.com'
-}))
+app.use(cors())
 
 app.use('/api/user/', userRouter)
 app.listen(PORT || 8000, async () => {
